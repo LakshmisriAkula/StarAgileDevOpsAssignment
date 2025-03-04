84b4d4ffc9290a9d23e17af74ae75d46df175c78
LakshmisriAkula <akulalakshmisri@gmail.com>
2025-03-04 12:55:10 +0530

    Initial

---
+++ src/main/java/StarAgile/DevopsAssignment/App.java
@@ -0,0 +1,10 @@
+package StarAgile.DevopsAssignment;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
